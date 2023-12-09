f99f1345eb6e22864af2f1a170b41a8777c31834
youjin Cho <ud071233@gmail.com>
2023-12-10 03:40:10 +0900

    Feat: User Entity 생성 및 자동 날짜 설정

---
+++ backend/booklog/src/main/java/com/booklog/booklog/common/domain/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package com.booklog.booklog.common.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@Getter
+@MappedSuperclass
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime updatedAt;
+}
