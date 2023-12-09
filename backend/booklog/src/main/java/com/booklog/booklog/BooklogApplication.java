32255446d75a7d92313e5f7d29cd4849664f8a7a
youjin Cho <ud071233@gmail.com>
2023-12-10 03:16:47 +0900

    Feat: BE 세팅

---
+++ backend/booklog/src/main/java/com/booklog/booklog/BooklogApplication.java
@@ -0,0 +1,15 @@
+package com.booklog.booklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class BooklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklogApplication.class, args);
+	}
+
+}
---
+++ backend/booklog/src/test/java/com/booklog/booklog/BooklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklog.booklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
