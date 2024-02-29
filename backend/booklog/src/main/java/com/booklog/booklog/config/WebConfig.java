43e0e2fc41feed1959966500bcbd5bfa708b122e
youjin Cho <ud071233@gmail.com>
2024-02-29 19:06:40 +0900

    Feat: CORS 에러 해결

--- backend/booklog/src/main/java/com/booklog/booklog/config/WebConfig.java
+++ backend/booklog/src/main/java/com/booklog/booklog/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.booklog.booklog.auth.domain.JwtTokenProvider;
 import com.booklog.booklog.auth.interceptor.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new JwtInterceptor(jwtTokenProvider))
                 .order(1)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
