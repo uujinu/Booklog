ca404a0279ea5c1621cfd8968291e5f90a8d8223
youjin Cho <ud071233@gmail.com>
2024-03-07 00:51:39 +0900

    Fix: 코드 수정

--- frontend/booklog/src/App.js
+++ frontend/booklog/src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/User/Login';
+import BookList from './components/book/BookList';
+import BookDetails from './components/book/BookDetails';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="book" element={<BookList />}></Route>
+      <Route path="/book/:id" element={<BookDetails />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
