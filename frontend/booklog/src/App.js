9f7fbf2e843cfe1857625ed273c233daa7bd6daa
youjin Cho <ud071233@gmail.com>
2024-03-08 00:30:56 +0900

    Feat: 로그인 여부에 따라 헤더메뉴 변경

--- frontend/booklog/src/App.js
+++ frontend/booklog/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/User/Login';
 import BookList from './components/book/BookList';
 import BookDetails from './components/book/BookDetails';
+import { MyPage } from './pages/User/MyPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="book" element={<BookList />}></Route>
       <Route path="/book/:id" element={<BookDetails />}></Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/my-page" element={<MyPage />}></Route>
     </Routes>
   );
 }
