92fbc21a901c12c5a6e75566dda51f7cd58b92fb
youjin Cho <ud071233@gmail.com>
2024-03-30 21:16:35 +0900

    Chore: Controller 이름 변경

---
+++ backend/booklog/src/main/java/com/booklog/booklog/domain/book/controller/SearchController.java
@@ -0,0 +1,26 @@
+package com.booklog.booklog.domain.book.controller;
+
+import com.booklog.booklog.common.response.ResponseDto;
+import com.booklog.booklog.domain.book.dto.SearchDetailDto;
+import com.booklog.booklog.domain.book.dto.SearchDto;
+import com.booklog.booklog.domain.book.service.BookService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/api/books")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SearchController {
+
+    private final BookService bookService;
+
+    @GetMapping("/search")
+    public ResponseEntity<ResponseDto<SearchDto>> search(@RequestParam("query") String query, @RequestParam("start") String start) {
+        return ResponseEntity.ok(ResponseDto.of(bookService.searchBook(query, start)));
+    }
+}
